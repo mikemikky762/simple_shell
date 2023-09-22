980b35b8f6e0e8c0fe9ea05fe288dc282436a8ec
Mayor129 <musefiu.muyiwa@yahoo.com>
2023-09-21 20:41:34 -0700

    Muyiwa Updated

--- main.h
+++ main.h
@@ -16,200 +16,218 @@
 #define TOK_BUFSIZE 128
 #define TOK_DELIM " \t\r\n\a"
 
-/* Declaration of the "environment" as an array of pointers to strings. */
+/* Points to an array of pointers to strings called the "environment" */
 extern char **environ;
 
+
 /**
-* struct data - Struct that holds essential runtime data.
-* @av: Argument vector.
-* @input: User-provided command line input.
-* @args: Tokenized arguments.
-* @status: Last shell status.
-* @counter: Line counter.
-* @_environ: Environment variables.
-* @pid: Process ID of the shell.
-*/
+ * struct data - struct that contains all relevant data on runtime
+ * @av: argument vector
+ * @input: command line written by the user
+ * @args: tokens of the command line
+ * @status: last status of the shell
+ * @counter: lines counter
+ * @_environ: environment variable
+ * @pid: process ID of the shell
+ */
 typedef struct data
 {
-char **av;
-char *input;
-char **args;
-int status;
-int counter;
-char **_environ;
-char *pid;
+	char **av;
+	char *input;
+	char **args;
+	int status;
+	int counter;
+	char **_environ;
+	char *pid;
 } data_shell;
 
 /**
-* struct sep_list_s - Single linked list to store separators.
-* @separator: Separator character (;, |, or &).
-* @next: Pointer to the next node.
-* Description: Used for managing command line separators.
-*/
+ * struct sep_list_s - single linked list
+ * @separator: ; | &
+ * @next: next node
+ * Description: single linked list to store separators
+ */
 typedef struct sep_list_s
 {
-char separator;
-struct sep_list_s *next;
+	char separator;
+	struct sep_list_s *next;
 } sep_list;
 
 /**
-* struct line_list_s - Single linked list to store command lines.
-* @line: Command line.
-* @next: Pointer to the next node.
-* Description: Used for storing command lines.
-*/
+ * struct line_list_s - single linked list
+ * @line: command line
+ * @next: next node
+ * Description: single linked list to store command lines
+ */
 typedef struct line_list_s
 {
-char *line;
-struct line_list_s *next;
+	char *line;
+	struct line_list_s *next;
 } line_list;
 
 /**
-* struct r_var_list - Single linked list to store variables.
-* @len_var: Length of the variable name.
-* @val: Value of the variable.
-* @len_val: Length of the value.
-* @next: Pointer to the next node.
-* Description: Used for managing runtime variables.
-*/
+ * struct r_var_list - single linked list
+ * @len_var: length of the variable
+ * @val: value of the variable
+ * @len_val: length of the value
+ * @next: next node
+ * Description: single linked list to store variables
+ */
 typedef struct r_var_list
 {
-int len_var;
-char *val;
-int len_val;
-struct r_var_list *next;
+	int len_var;
+	char *val;
+	int len_val;
+	struct r_var_list *next;
 } r_var;
 
 /**
-* struct builtin_s - Builtin struct for command arguments.
-* @name: The name of the builtin command (e.g., cd, exit, env).
-* @f: Pointer to the corresponding function.
-*/
+ * struct builtin_s - Builtin struct for command args.
+ * @name: The name of the command builtin i.e cd, exit, env
+ * @f: data type pointer function.
+ */
 typedef struct builtin_s
 {
-char *name;
-int (*f)(data_shell *datash);
+	char *name;
+	int (*f)(data_shell *datash);
 } builtin_t;
 
-/* Function prototypes for various parts of the code. */
-
-/* Functions related to linked lists. */
+/* aux_lists.c */
 sep_list *add_sep_node_end(sep_list **head, char sep);
 void free_sep_list(sep_list **head);
 line_list *add_line_node_end(line_list **head, char *line);
 void free_line_list(line_list **head);
+
+/* aux_lists2.c */
 r_var *add_rvar_node(r_var **head, int lvar, char *var, int lval);
 void free_rvar_list(r_var **head);
 
-/* String manipulation functions. */
+/* aux_str functions */
 char *_strcat(char *dest, const char *src);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
-void rev_string(char *s);
 
-/* Memory management functions. */
+/* aux_mem.c */
 void _memcpy(void *newptr, const void *ptr, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
+
+/* aux_str2.c */
 char *_strdup(const char *s);
 int _strlen(const char *s);
 int cmp_chars(char str[], const char *delim);
 char *_strtok(char str[], const char *delim);
 int _isdigit(const char *s);
 
-/* Syntax error checking functions. */
+/* aux_str3.c */
+void rev_string(char *s);
+
+/* check_syntax_error.c */
 int repeated_char(char *input, int i);
 int error_sep_op(char *input, int i, char last);
 int first_char(char *input, int *i);
 void print_syntax_error(data_shell *datash, char *input, int i, int bool);
 int check_syntax_error(data_shell *datash, char *input);
 
-/* Main shell loop functions. */
+/* shell_loop.c */
 char *without_comment(char *in);
 void shell_loop(data_shell *datash);
 
-/* Reading user input functions. */
+/* read_line.c */
 char *read_line(int *i_eof);
 
-/* Command line splitting functions. */
+/* split.c */
 char *swap_char(char *input, int bool);
 void add_nodes(sep_list **head_s, line_list **head_l, char *input);
 void go_next(sep_list **list_s, line_list **list_l, data_shell *datash);
 int split_commands(data_shell *datash, char *input);
 char **split_line(char *input);
 
-/* Variable replacement functions. */
+/* rep_var.c */
 void check_env(r_var **h, char *in, data_shell *data);
 int check_vars(r_var **h, char *in, char *st, data_shell *data);
 char *replaced_input(r_var **head, char *input, char *new_input, int nlen);
 char *rep_var(char *input, data_shell *datash);
 
-/* Reading lines from input. */
+/* get_line.c */
 void bring_line(char **lineptr, size_t *n, char *buffer, size_t j);
 ssize_t get_line(char **lineptr, size_t *n, FILE *stream);
 
-/* Executing shell commands. */
+/* exec_line */
 int exec_line(data_shell *datash);
+
+/* cmd_exec.c */
 int is_cdir(char *path, int *i);
 char *_which(char *cmd, char **_environ);
 int is_executable(data_shell *datash);
 int check_error_cmd(char *dir, data_shell *datash);
 int cmd_exec(data_shell *datash);
 
-/* Environment variable functions. */
+/* env1.c */
 char *_getenv(const char *name, char **_environ);
 int _env(data_shell *datash);
 
-/* Environment manipulation functions. */
+/* env2.c */
 char *copy_info(char *name, char *value);
 void set_env(char *name, char *value, data_shell *datash);
 int _setenv(data_shell *datash);
 int _unsetenv(data_shell *datash);
 
-/* Directory change functions. */
+/* cd.c */
 void cd_dot(data_shell *datash);
 void cd_to(data_shell *datash);
 void cd_previous(data_shell *datash);
 void cd_to_home(data_shell *datash);
 
-/* Built-in command lookup function. */
+/* cd_shell.c */
+int cd_shell(data_shell *datash);
+
+/* get_builtin */
 int (*get_builtin(char *cmd))(data_shell *datash);
 
-/* Exit shell function. */
+/* _exit.c */
 int exit_shell(data_shell *datash);
 
-/* Additional utility functions. */
+/* aux_stdlib.c */
 int get_len(int n);
 char *aux_itoa(int n);
 int _atoi(char *s);
 
-/* Error message functions. */
+/* aux_error1.c */
 char *strcat_cd(data_shell *, char *, char *, char *);
 char *error_get_cd(data_shell *datash);
 char *error_not_found(data_shell *datash);
 char *error_exit_shell(data_shell *datash);
+
+/* aux_error2.c */
 char *error_get_alias(char **args);
 char *error_env(data_shell *datash);
 char *error_syntax(char **args);
 char *error_permission(char **args);
 char *error_path_126(data_shell *datash);
 
-/* Error handling functions. */
+
+/* get_error.c */
 int get_error(data_shell *datash, int eval);
 
-/* Signal handling function. */
+/* get_sigint.c */
 void get_sigint(int sig);
 
-/* Help message functions. */
+/* aux_help.c */
 void aux_help_env(void);
 void aux_help_setenv(void);
 void aux_help_unsetenv(void);
 void aux_help_general(void);
 void aux_help_exit(void);
+
+/* aux_help2.c */
 void aux_help(void);
 void aux_help_alias(void);
 void aux_help_cd(void);
+
+/* get_help.c */
+int get_help(data_shell *datash);
 
 #endif
